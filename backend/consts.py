410926efed1b5d8e53c9c19a17d12d4166f4a20e
Yosuke Konno <yokohey@outlook.com>
2023-01-20 21:37:35 +0900

    update temperature

--- backend/consts.py
+++ backend/consts.py
@@ -15,7 +15,10 @@ VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
 # We can customize generation parameters (see https://huggingface.co/blog/how-to-generate)
 GEN_TOP_K = None
 GEN_TOP_P = None
-TEMPERATURE  = None
+TEMPERATURE  = 1.0 # None 
+# https://github.com/borisdayma/dalle-mini/issues/249
+# seems hugging face was using 1.0
+
 COND_SCALE = 10.0
 
 class ModelSize(Enum):
