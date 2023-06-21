8a068eb00ea70be76edff064527de5cdc63a85a5
Yosuke Konno <yokohey@outlook.com>
2023-06-21 15:31:03 +0900

    test change to release version

--- backend/consts.py
+++ backend/consts.py
@@ -5,7 +5,7 @@ DEFAULT_IMG_OUTPUT_DIR = 'generations'
 DALLE_MODEL_MINI = "dalle-mini/dalle-mini/mini-1:v0"  # the original DALL-E Mini. Fastest yet suboptimal results
 DALLE_MODEL_MEGA = "dalle-mini/dalle-mini/mega-1-fp16:latest"  # the advanced version of DALL-E Mini. Requires more compute and VRAM
 DALLE_MODEL_MEGA_FULL = "dalle-mini/dalle-mini/mega-1:latest"  # DALL-E Mega. Warning: requires significantly more storage and GPU RAM
-DALLE_COMMIT_ID = "80171af98ff0b7c3ff540f8cfcc77baa19f1c628" # None
+DALLE_COMMIT_ID = "90537ecffcf6238d0569ae537c71f5afa77d76e3" # None
 
 # VQGAN model
 VQGAN_REPO = "dalle-mini/vqgan_imagenet_f16_16384"
@@ -15,7 +15,7 @@ VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
 # We can customize generation parameters (see https://huggingface.co/blog/how-to-generate)
 GEN_TOP_K = None
 GEN_TOP_P = None
-TEMPERATURE  = None # 1.0 # None 
+TEMPERATURE  = 1.0 # None 
 # https://github.com/borisdayma/dalle-mini/issues/249
 # seems hugging face was using 1.0
 # changing to None again
