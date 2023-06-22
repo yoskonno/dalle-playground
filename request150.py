c63b4838c887ab96dc265da7f69ba089c80b0edc
Yosuke Konno <yokohey@outlook.com>
2023-06-22 10:15:36 +0900

    update temperature again

---
+++ request150.py
@@ -0,0 +1,22 @@
+import requests
+import json
+from datetime import datetime
+
+num_requests = 50
+i = 1
+
+# "a real skull on black background"
+# "a human skull on black background","num_images"
+
+CLOUDFLARE_URL = 'https://lawn-rom-teach-philadelphia.trycloudflare.com'
+
+while i <= num_requests:
+    data = {"text":"skull on a black background","num_images":8}
+    headers = {'Content-Type': 'application/json'}
+
+    response = requests.post(CLOUDFLARE_URL + '/dalle', data=json.dumps(data), headers=headers)
+
+    # parse the response and check if you need to continue making requests
+    print(f"{response.json()} {i}/{num_requests} - {datetime.now().strftime('%H:%M:%S %d-%m-%Y')}")
+
+    i += 1
